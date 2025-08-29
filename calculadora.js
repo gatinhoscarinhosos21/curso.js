04c1e67b4471c85fe053254a50afc4fee576f1f2
gatinhoscarinhosos21 <a.alves21@escola.pr.gov.br>
2025-08-29 12:15:00 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+//Declarando variáveis
+let numero1 = 10;
+let numero2 = 5;
+
+//Operações matemáticas
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+//Exibindo resultados no console
+console.log("numero1: ", numero1);
+console.log("numero2: ",  numero2);
+console.log("Soma: ", soma);
+console.log("Subtração: ", subtracao);
+console.log("Multiplicação: ", multiplicacao);
+console.log("Divisão: ", divisao);
---
+++ exer2.js
@@ -0,0 +1,13 @@
+// Lista de ingredientes de bolo (array)
+const ingredientes = [ 
+    "3 ovos",
+    "2 xícaras de farinha de trigo",
+    "1 xícara de açúcar",
+    "1/2 xícara de óleo",
+    "1 xícara de leite",
+    "1 colher de sopa de fermento em pó"
+];
+
+//título
+console.log("Receita de Bolo Simples");
+console.log(ingredientes.join
